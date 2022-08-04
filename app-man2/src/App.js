d89e82e961cba8806d67db1e8fa53add520df3a8
manolte96 <michaelnolte13@gmail.com>
2022-08-04 18:04:21 -0500

    will come back to this

--- app-man2/src/App.js
+++ app-man2/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from "react"
 function App() {
+  const [isClicked, setIsClicked] = useState(false)
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const Gone = () => {
+//remove this item from toDo array
+  }
+  console.log("Your state is",isClicked)
+  console.log(toDo)
+  console.log(toDos)
+
+  const onChangeHandler = (e) =>{
+    setToDo(e.target.value)
+  }
+  const onClickHandler = () => {
+  // Make a copy of the todo state
+  setToDos([...toDos, toDo])
+  setToDo("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Howdy Y'all
+      <br></br>
+      <button onClick = {onClickHandler}>"Click Me!"
+      </button> <br></br>
+      <input onChange = {onChangeHandler} value = {toDo}>
+      </input>
+      <ul>
+  {toDos.map(toDo => {
+    return (
+      <li onClick = {Gone}
+       key={toDo.id}>
+        {toDo}
+      </li>)
+  })}
+</ul>
     </div>
   );
 }
